200a5932e7490fe98b644bf0b9f8bd4b6a4b9de0
Arun Kumar <arunraj91221@gmail.com>
2026-05-25 22:39:56 +0530

    Added If Demo program

---
+++ Control flow/IfDemo.java
@@ -0,0 +1,12 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        int age = 18;
+        
+        // Simple if
+        if (age >= 18) {
+            System.out.println("You can vote!");
+        }
+        
+        
+    }
+}
